60f99178ccbd2c4e59083bacd8652ebea2216ad7
Anthony Gabriel <anthonygabriel642@gmail.com>
2022-08-03 17:19:32 -0300

    Modificação das classes Models

--- SubjectManager.Api/Models/Lesson.cs
+++ SubjectManager.Api/Models/Lesson.cs
@@ -5,7 +5,16 @@ public class Lesson
 {
     [Key]
     public int Id { set; get; }
-    public string? DayOfWeak { set; get; }
-    public string? StartTime { set; get; }
-    public string? EndTime { set; get; }
+
+    [Required(ErrorMessage = "Este campo é obrigatorio")]
+    public int SubjectId { set; get; }
+
+    [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
+    public string DayOfWeak { set; get; } = null!;
+
+    [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
+    public string StartTime { set; get; } = null!;
+    
+    [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
+    public string EndTime { set; get; } = null!;
 }
--- SubjectManager.Api/Models/Subject.cs
+++ SubjectManager.Api/Models/Subject.cs
@@ -5,7 +5,11 @@ public class Subject
 {
     [Key]
     public int Id { set; get; }
-    public string? Name { set; get; }
+
+    [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
+    [MaxLength(25, ErrorMessage = "Esta campo deve ter no mpaximo 35 caracteres")]
+    public string Name { set; get; } = null!;
+    
     public List<Lesson>? Lessons { set; get; }
 
 }
