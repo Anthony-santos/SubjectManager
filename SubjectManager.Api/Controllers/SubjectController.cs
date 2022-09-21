ced49e2e40ebaf80825a33adc46165880e9c0d00
ThonySantos <anthonygabriel642@gmail.com>
2022-09-21 16:42:04 -0300

    refactor: update controller to async methods

--- SubjectManager.Api/Controllers/SubjectController.cs
+++ SubjectManager.Api/Controllers/SubjectController.cs
@@ -4,70 +4,86 @@ namespace SubjectManager.Api.Controllers;
 [Route("[controller]")]
 public class SubjectController : ControllerBase
 {
-    private readonly ISubjectRepository _subjectRepository;
-    public SubjectController(ISubjectRepository subjectRepository)
+    private readonly ISubjectService _subjectService;
+
+    public SubjectController(ISubjectService subjectService)
     {
-        _subjectRepository = subjectRepository;
+        _subjectService = subjectService;
     }
 
-    [HttpGet]
-    [Route("")]
-    public IActionResult GetAll()
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
     {
-        var subjects = _subjectRepository.GetAllSubjects();
-        return Ok(subjects);
-    }
-
-    [HttpGet]
-    [Route("{id:int}")]
-    public IActionResult GetById(int id)
-    {
-        var subject = _subjectRepository.GetSubjectById(id);
-        return Ok(subject);
-    }
-
-    [HttpPost]
-    [Route("")]
-    public IActionResult Create([FromBody] Subject model)
-    {
-        if(ModelState.IsValid)
+        try
         {
-            try
-            {
-                var subject = _subjectRepository.AddSubject(model);
-                return Ok(subject);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            var subjects = await _subjectService.GetAllSubjectsAsync();
+            return Ok(subjects);
         }
-        return StatusCode(412, "Ouver um erro ao tentar criar uma nova matéria");
-    }
-    [HttpPost]
-    [Route("/edit")]
-    public IActionResult Edit([FromBody] Subject model)
-    {
-        if (ModelState.IsValid)
+        catch (Exception ex)
         {
-            try
-            {
-                var subject = _subjectRepository.EditSubject(model);
-                return Ok(subject);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }            
+            return BadRequest(new List<string> { ex.Message });
         }
-        return StatusCode(412, "Ouve um erro ao tentar editar a matéria.");
     }
 
-    [HttpDelete]
-    [Route("{id:int}")]
-    public IActionResult Delete(int id)
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
     {
-        var subject = _subjectRepository.RemoveSubjectById(id);
-        return Ok(subject);
+        try
+        {
+            var subject = await _subjectService.GetSubjectByIdAsync(id);
+            return Ok(subject);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new List<string> { ex.Message });
+        }
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> Create([FromBody] Subject model)
+    {
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState.Values.SelectMany(v => v.Errors.Select(b => b.ErrorMessage)));
+
+        try
+        {
+            var subject = await _subjectService.AddSubjectAsync(model);
+            return Ok(subject);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new List<string> { ex.Message });
+        }
+    }
+
+    [HttpPost("edit/{id:int}")]
+    public async Task<IActionResult> Edit(int id, [FromBody] Subject model)
+    {
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState.Values.SelectMany(v => v.Errors.Select(b => b.ErrorMessage)));
+
+        try
+        {
+            var subject = await _subjectService.EditSubject(id, model);
+            return Ok(subject);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new List<string> { ex.Message });
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            await _subjectService.RemoveSubjectById(id);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new List<string> { ex.Message });
+        }
     }
 }
