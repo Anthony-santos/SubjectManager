404df60e8bb92fe92a1ca6dcf7a5825dfcde5f0e
ThonySantos <anthonygabriel642@gmail.com>
2022-09-21 16:41:32 -0300

    docs: add global using

--- SubjectManager.Api/GlobalUsing.cs
+++ SubjectManager.Api/GlobalUsing.cs
@@ -1,4 +1,5 @@
-﻿global using SubjectManager.Domain.Models;
+﻿global using SubjectManager.Api.Services.Interfaces;
+global using SubjectManager.Domain.Models;
 global using SubjectManager.Infra.Repositories;
 global using SubjectManager.Infra.Repositories.Interfaces;
 global using SubjectManager.Infra.Context;
