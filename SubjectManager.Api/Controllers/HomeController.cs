4c2eb7c4c5558064a8b2e9d9808ba7e3a586a774
ThonySantos <anthonygabriel642@gmail.com>
2023-07-27 17:09:37 -0300

    feat: add api

---
+++ SubjectManager.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SubjectManager.Api.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+    
+}
