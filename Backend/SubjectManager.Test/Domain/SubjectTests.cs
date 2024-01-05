f04dbc169e563f337b67a967a1025f3697a1239e
ThonySantos <anthonygabriel642@gmail.com>
2024-01-05 10:01:40 -0300

    ✅refactor: update test methods

--- Backend/SubjectManager.Test/Domain/SubjectTests.cs
+++ Backend/SubjectManager.Test/Domain/SubjectTests.cs
@@ -9,7 +9,7 @@ public class SubjectTests
     [TestMethod]
     public void Should_Return_Error_When_Trying_To_Create_An_Subject_With_Invalid_Name()
     {
-        var subject = new Subject("P");
+        var subject = new Subject("");
         Assert.IsFalse(subject.IsValid);
     }
 
@@ -19,17 +19,15 @@ public class SubjectTests
         var subject = new Subject("Meus pais morreram, e agora?");
         subject.AddLesson(_smallLesson);
         subject.AddLesson(_smallLesson);
-        var totalSubjectLesson = subject.Lessons.Count();
-        Assert.AreEqual(1, totalSubjectLesson);
+        Assert.IsFalse(subject.IsValid);
     }
 
     [TestMethod]
     public void Should_Return_Error_When_Trying_To_Put_One_Class_Inside_Other()
     {
-        var subject = new Subject("Batman 101");
+        var subject = new Subject("Introdução ao combate do crime");
         subject.AddLesson(_smallLesson);
         subject.AddLesson(_bigLesson);
-        var totalSubjectLesson = subject.Lessons.Count();
-        Assert.AreEqual(1, totalSubjectLesson);
+        Assert.IsFalse(subject.IsValid);
     }
 }
